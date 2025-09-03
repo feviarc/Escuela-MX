65bc324c452d241e605ab722a5920d65168e9125
Felipe Víctor <felipe@oitsys.com>
2025-09-03 17:14:25 -0600

    chore(firebase): add @angular/fire to project

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "escuela-mx", appId: "1:913432788471:web:439bd307de5889e5ec02f2", storageBucket: "escuela-mx.firebasestorage.app", apiKey: "AIzaSyCbb5XIbL6lWA98J2uhfguoDvwxgfiYbJo", authDomain: "escuela-mx.firebaseapp.com", messagingSenderId: "913432788471" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });

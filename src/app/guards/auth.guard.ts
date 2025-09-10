72ca56d0f8bf556fbd6f476048f6e6c116698c83
Felipe Víctor <felipe@oitsys.com>
2025-09-09 19:50:56 -0600

    feat(guards): implement authGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,47 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+import { Observable, map, of, switchMap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { UserProfileService } from '../services/user-profile.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+@Injectable({providedIn: 'root'})
+
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private userProfileService: UserProfileService,
+    private router: Router
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+
+    const expectedRole = route.data['expectedRole'];
+
+    return this.authService.getCurrentUser().pipe(
+      switchMap(user => {
+
+        if(!user) {
+          return of(this.router.createUrlTree(['/portal']));
+        }
+
+        return this.userProfileService.getUserProfile(user.uid).pipe(
+          map(profile => {
+            if(profile && profile.rol === expectedRole) {
+              return true;
+            } else {
+              return this.router.createUrlTree(['/portal']);
+            }
+          })
+        );
+      })
+    );
+  }
+}

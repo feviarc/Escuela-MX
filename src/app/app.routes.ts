6c5a1e1ecb25875f25591ecf84125bab6fc5d8cb
Felipe Víctor <felipe@oitsys.com>
2025-09-10 19:24:49 -0600

    chore: add AdminPage with CLI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,4 +18,8 @@ export const routes: Routes = [
     path: 'auth',
     loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
   },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin.page').then( m => m.AdminPage)
+  },
 ];

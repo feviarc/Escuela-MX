afe8e99cde6d928691b92f887cfca4b4481ecea3
Felipe Víctor <felipe@oitsys.com>
2025-09-11 18:47:39 -0600

    feat(teacher-dashboard): implement TeacherPage logic

--- src/app/teacher/teacher.page.ts
+++ src/app/teacher/teacher.page.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from  '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
+import { AuthService } from '../services/auth.service';
+
 
 @Component({
-  selector: 'app-teacher',
+  selector: 'app-maestro',
   templateUrl: './teacher.page.html',
   styleUrls: ['./teacher.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, IonicModule]
 })
+
 export class TeacherPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) {
+    addIcons({logOutOutline});
+  }
 
   ngOnInit() {
   }
 
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigateByUrl('/auth');
+      },
+      error: error => {
+        console.log('Error de cierre de sesión: ', error);
+      }
+    });
+  }
 }

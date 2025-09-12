7edeca7b25fd0c1144f4dedd9a49e9d615b337dd
Felipe Víctor <felipe@oitsys.com>
2025-09-11 18:40:19 -0600

    feat(admin-dashboard): implement AdminPage logic

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { TeacherDataService } from '../services/teacher-data.service';
+import { UserProfile } from '../models/user-profile.model';
+
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
   styleUrls: ['./admin.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, IonicModule]
 })
+
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  teachers$!: Observable<UserProfile[]>;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private teacherDataService: TeacherDataService
+  ) {
+    addIcons({logOutOutline});
   }
 
+  ngOnInit() {
+    this.teachers$ = this.teacherDataService.getTeachers();
+  }
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigateByUrl('/auth');
+      },
+      error: error => {
+        console.log('Error al cerrar sesión: ',  error);
+      }
+    });
+  }
 }

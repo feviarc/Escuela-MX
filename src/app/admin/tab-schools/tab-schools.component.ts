f0b78f3d579a9a7f8e55ca4ad7e76ef373ba04ee
Felipe Víctor <felipe@oitsys.com>
2025-10-26 15:53:21 -0600

    feat(admin-dashboard): add schools list to tab-schools

--- src/app/admin/tab-schools/tab-schools.component.ts
+++ src/app/admin/tab-schools/tab-schools.component.ts
@@ -1,10 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
 import {
+  IonChip,
   IonContent,
+  IonFab,
+  IonFabButton,
   IonHeader,
-  IonToolbar,
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonProgressBar,
   IonTitle,
+  IonToolbar,
 } from "@ionic/angular/standalone";
+
+import { Subscription } from 'rxjs';
+import { SchoolCRUDService, School } from 'src/app/services/school-crud.service';
 
 
 @Component({
@@ -13,17 +29,54 @@ import {
   styleUrls: ['./tab-schools.component.scss'],
   standalone: true,
   imports: [
+    CommonModule,
+    IonChip,
     IonContent,
+    IonFab,
+    IonFabButton,
     IonHeader,
-    IonToolbar,
+    IonIcon,
+    IonItem,
+    IonItemOption,
+    IonItemOptions,
+    IonItemSliding,
+    IonLabel,
+    IonList,
+    IonProgressBar,
     IonTitle,
+    IonToolbar,
   ]
 })
 
-export class TabSchoolsComponent  implements OnInit {
+export class TabSchoolsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isLoading: boolean = true;
+  schools: School[] = [];
+  private subscription?: Subscription;
 
-  ngOnInit() {}
+  constructor(private schoolCRUDService: SchoolCRUDService) {}
 
+  ngOnInit() {
+    this.subscription = this.schoolCRUDService.schools$.subscribe({
+      next: schools => {
+        this.schools = schools;
+        if(schools.length !== 0){
+          this.isLoading = false;
+        }
+      },
+      error: (e) => {
+        console.log('Error: ', e);
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    if(this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  onAddSchool() {
+    console.log(Date.now());
+  }
 }
--- src/app/admin/tab-users/tab-users.component.ts
+++ src/app/admin/tab-users/tab-users.component.ts
@@ -11,7 +11,6 @@ import {
   IonItem,
   IonLabel,
   IonList,
-  IonListHeader,
   IonProgressBar,
   IonToolbar,
   IonTitle,
